7878e1d30431b1118cc63da5f2461861dc03a0cd
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-02-06 16:04:13 +0530

    Day 3 now complete

--- day_3/script.js
+++ day_3/script.js
@@ -1,109 +1,109 @@
 // 1. Print square star pattern
 
-// let row = 5;
-// let col = 5;
+let row = 5;
+let col = 5;
 
-// for (let i = 1; i <= row; i++){
-//     let star = ""
-//     for (let j = 1; j <= col; j++){
-//         star += "* "
-//     }
-//     console.log(star)
-// }
+for (let i = 1; i <= row; i++){
+    let star = ""
+    for (let j = 1; j <= col; j++){
+        star += "* "
+    }
+    console.log(star)
+}
 
 // 2. Print right angle star pattern
 
-// let row = 5;
-// let col = 5;
+let row = 5;
+let col = 5;
 
-// for (let i = 1; i <= row; i++){
-//     let star = ""
-//     for (let j = 1; j <= i; j++){
-//         star += "* "
-//     }
-//     console.log(star)
-// }
+for (let i = 1; i <= row; i++){
+    let star = ""
+    for (let j = 1; j <= i; j++){
+        star += "* "
+    }
+    console.log(star)
+}
 
-// 3 Print inverted right angle star pattern
+3 Print inverted right angle star pattern
 
-//  let row = 5;
-// let col = 5;
+ let row = 5;
+let col = 5;
 
-// for (let i = row; i > 0; i--){
-//     let star = ""
-//     for (let j = 1; j <= i ; j++){
-//         star += "* "
-//     }
-//     console.log(star)
-// }
+for (let i = row; i > 0; i--){
+    let star = ""
+    for (let j = 1; j <= i ; j++){
+        star += "* "
+    }
+    console.log(star)
+}
 
-// 4 Print piramid pattern
+4 Print piramid pattern
 
-//  let row = 5;
+ let row = 5;
 
 
-// for (let i = 1; i <= row; i++){
+for (let i = 1; i <= row; i++){
 
-//     let space = ""
-//     for (let z = 1; z <= row - i; z++){
-//         space += "  "
-//     }
+    let space = ""
+    for (let z = 1; z <= row - i; z++){
+        space += "  "
+    }
 
-//     let star = ""
-//     for (let j = 1; j <= i * 2 - 1; j++){
-//         star += "* "
-//     }
-//     console.log(space,star)
-// }
+    let star = ""
+    for (let j = 1; j <= i * 2 - 1; j++){
+        star += "* "
+    }
+    console.log(space,star)
+}
 
 // 2nd way
 
-// let row = 5;
-// for (let i = 1; i <= row; i++) {
-// 	let space = "  ".repeat(row - i);
-// 	let star = "* ".repeat(i * 2 - 1);
-// 	console.log(space + star);
-// }
+let row = 5;
+for (let i = 1; i <= row; i++) {
+	let space = "  ".repeat(row - i);
+	let star = "* ".repeat(i * 2 - 1);
+	console.log(space + star);
+}
 
 
 // 5 . Reverse piramid
 
 
-// let row = 5;
+let row = 5;
 
-// for (let i = row; i >= 1; i--){
+for (let i = row; i >= 1; i--){
 
-//     let space = ""
-//     for (let z = i; z < row ; z++){
-//         space += "  "
-//     }
+    let space = ""
+    for (let z = i; z < row ; z++){
+        space += "  "
+    }
 
-//     let star = ""
-//     for (let j = 1; j <= i * 2 - 1; j++){
-//         star += "* "
-//     }
-//     console.log(space,star)
-// }
+    let star = ""
+    for (let j = 1; j <= i * 2 - 1; j++){
+        star += "* "
+    }
+    console.log(space,star)
+}
 
 
 
 // 6 Hollow square
 
-//  let row = 7;
-// let col = 7;
+ let row = 7;
+let col = 7;
 
-// for (let i = 1; i <= row; i++){
-//     let star = ""
+for (let i = 1; i <= row; i++){
+    let star = ""
 
-//     for (let j = 1; j <= col; j++){
-//         if (i === 1 || i === row || j === 1 || j === col) {
-//             star += "* "
-//         } else {
-//             star += "  "
-//         }
-//     }
-//     console.log(star)
-// }
+    for (let j = 1; j <= col; j++){
+        if (i === 1 || i === row || j === 1 || j === col) {
+            star += "* "
+        } else {
+            star += "  "
+        }
+    }
+    console.log(star)
+}
 
 
 // 7 Hollow piramid
