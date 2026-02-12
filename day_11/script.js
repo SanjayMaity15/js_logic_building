9c0e08628b82437be448706e1745cc9486b938bd
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-02-12 16:36:49 +0530

    Today i learned kadane also in detais

--- day_11/script.js
+++ day_11/script.js
@@ -114,4 +114,90 @@ function findMaximumSumOfLongSubArrayKadanes(arr) {
     return maxSum
 }
 
-console.log(findMaximumSumOfLongSubArrayKadanes([1, 4, -5, 7, -3]))
+// console.log(findMaximumSumOfLongSubArrayKadanes([1, 4, -5, 7, -3]))
+
+function findMaxSubUsingAndReturnArrayAlso(arr) {
+	let maxSum = arr[0];
+	let currentsum = arr[0]
+
+	let start = 0, end = 0, tempStart = 0;
+
+	for (let i = 1; i < arr.length; i++){
+		if (arr[i] > arr[i] + currentsum) {
+			currentsum = arr[i];
+			tempStart = i;
+		} else {
+			currentsum += arr[i];
+		}
+
+
+		if (currentsum > maxSum) {
+			maxSum = currentsum;
+			start = tempStart;
+			end = i
+		}
+
+	}
+
+	const maxSubArray = arr.slice(start, end + 1)
+
+	return {maxSubArray, maxSum}
+}
+
+console.log(findMaxSubUsingAndReturnArrayAlso([-2, 4, 7, -5, 67, -345, 2, 12, -5634, 3, 3533,  3, -4, 56]))
+
+// H/W
+
+// Find the longest subarray with sum = k
+// input: [1, 2, 3, 4, 5] K = 9
+// output: [2, 3, 4] = 9
+
+function longestSubArrayWithSumK(arr, k) {
+	
+	let maxLength = 0
+	let maxSubArray;
+
+	for (let i = 0; i < arr.length; i++){
+		for (let j = i + 1; j <= arr.length; j++){
+			let subArray = arr.slice(i, j);
+			let sum = 0;
+			for (let i = 0; i < subArray.length; i++){
+				sum+= subArray[i]
+			}
+
+			if (sum === k && subArray.length > maxLength) {
+				maxLength = subArray.length;
+				maxSubArray = subArray;
+			}
+		}
+	}
+
+	return {maxSubArray, maxLength}
+
+
+}
+
+console.log(longestSubArrayWithSumK([1, 2, 3, 4, 5], 9))
+
+
+
+
+// Find the length of longest subarray with no repeating element
+// input: [1, 2, 3, 1, 2, 3, 4]
+// output: 4
+
+
+
+
+// maximum subarray (kadane algo)
+// input: [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+// output: [4, -1, 2, 1]
+
+
+
+
+// Find the longest incresing subarray(continuous)
+
+// input: [1, 2, 2, 3, 4, 5]
+// output: length = 4, subarray = [2, 3, 4, 5]
+
